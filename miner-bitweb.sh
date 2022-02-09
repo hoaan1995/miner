00fd1d26013625971627d2b42020c07f79fe080e
hoaan1995 <91445089+hoaan1995@users.noreply.github.com>
2022-02-09 08:37:03 -0800

    Add files via upload

---
+++ miner-bitweb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+while [ 1 ]; do
+"$FOLDER"/cpuminer-sse2 -a yespower -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u WALLET.WORKER_NAME
+sleep 5
+done
